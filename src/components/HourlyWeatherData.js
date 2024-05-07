d43038267aa0b0e8eb01ced6fc8d0659f4b594bd
aycin <aycinkaya98@gmail.com>
2024-05-07 15:02:23 +0100

    Completed details for hourly weather data with some styling. Next step is to add functionality to go back to daily forecasts

--- src/components/HourlyWeatherData.js
+++ src/components/HourlyWeatherData.js
@@ -1,18 +1,17 @@
 import React from "react";
-import "./WeatherData.css";
+import "./HourlyWeatherData.css";
 
 export default function WeatherData({ forecast, indexOfClicked }) {
   const forecastHours = forecast.forecast.forecastday[indexOfClicked].hour;
-  console.log(forecastHours);
 
   function mapForecastHours() {
     return forecastHours.map((hour, index) => {
       const date = new Date(hour.time);
       return (
-        <div className="hourly-forecast-card" key={index}>
-          <div className={`forecast-${hour}`}>
+        <div className={`hourly-forecast-card`} key={index}>
+          <div className={`forecast-${index}`}>
             <div className="time">
-              <h6>
+              <p>
                 {date
                   .toString()
                   .split(" ")
@@ -20,7 +19,25 @@ export default function WeatherData({ forecast, indexOfClicked }) {
                   .split(":")
                   .splice(0, 2)
                   .join(":")}
-              </h6>
+              </p>
+            </div>
+            <div className="section--1">
+              <div className="condition">{hour.condition.text}</div>
+              <img
+                src="//cdn.weatherapi.com/weather/64x64/night/113.png"
+                alt="condition-icon"
+              />
+              <div className="temp">{hour.temp_c} °C</div>
+            </div>
+            <div className="feels-like-temp">
+              Feels like {hour.feelslike_c} °C
+            </div>
+            <div className="humidity">Humidity: {hour.humidity}%</div>
+            <div className="precipitation-percentage">
+              {hour.chance_of_rain}% chance of rain
+            </div>
+            <div className="wind">
+              Wind: {hour.wind_dir} at {hour.wind_mph} mph
             </div>
           </div>
         </div>
@@ -28,5 +45,10 @@ export default function WeatherData({ forecast, indexOfClicked }) {
     });
   }
 
-  return <div>{mapForecastHours()}</div>;
+  return (
+    <div>
+      <p>{forecast.forecast.forecastday.date}</p>
+      <div className="forecast-cards">{mapForecastHours()}</div>
+    </div>
+  );
 }
